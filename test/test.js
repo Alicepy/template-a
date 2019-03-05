d0813470da287895a06dc550502087499c92bdcc
chenpeiyu <chenpeiyu@gosuncn.com>
2019-03-05 20:11:56 +0800

    changed cpy 增加karma测试

---
+++ test/test.js
@@ -0,0 +1,8 @@
+describe('加法函数的测试', function () {
+    it('1 加 1 应该等于 2', function () {
+        expect(true).toBe(true);
+    });
+    it('2 加 2 应该等于 4', function () {
+        expect(true).toBe(true);
+    });
+});
